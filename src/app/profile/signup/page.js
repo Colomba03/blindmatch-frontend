79fd5b1a26fe1ac106724921aee3d335a9ab7fab
lacc27 <luiscc@haipriority.com>
2024-04-27 17:48:58 -0400

    implemented login/signup, added routes to menu and env

--- src/app/profile/signup/page.js
+++ src/app/profile/signup/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Chip, Link, TextField } from "@mui/material";
+import { Button, Chip, Link, MenuItem, Select, TextField } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { LocalizationProvider } from '@mui/x-date-pickers';
@@ -7,6 +7,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from "dayjs";
 import Papa from "papaparse";
+import { getCookie, setCookie } from "cookies-next";
 // import {promise as fs} from 'fs';
 // import * as hobbies from "../../../../public/data/hobbies.csv";
 
@@ -43,14 +44,16 @@ export default function SignUpPage() {
   const [email, setEmail] = useState("");
   const [fname,setFname] = useState("");
   const [lname, setLname] = useState("");
+  const [sex, setSex] = useState("");
   const [interests,setInterests] = useState([]);
   const [selectedInts,setSelectedInts] = useState([]);
   const router = useRouter();
-  const birthday = new Date();
+  const [birthday,setBirthday] = useState(new Date());
+  const [uid,setUid] = useState(0);
 
   async function readInterests(){
     const file = "../../../../public/data/hobbies.csv";
-    await fetch(`http://localhost:3003/interest/getHobbies`)
+    await fetch(`${process.env.NEXT_PUBLIC_URL}/interest/getHobbies`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
@@ -63,6 +66,49 @@ export default function SignUpPage() {
     //     console.log(results);
     //   }
     // });
+
+  }
+
+  function check(){
+    if(username != '' && fname != '' && email != '' && password != '' && password == confirm && birthday != null && sex != ''){
+      return true
+    }
+    return false
+  }
+
+  async function setHobbies(){
+    const file = "../../../../public/data/hobbies.csv";
+    const id = getCookie("user_id");
+    console.log(id);
+    await fetch(`${process.env.NEXT_PUBLIC_URL}/interest/selectHobbies/${id}`, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(selectedInts),
+      headers: { "Content-Type": "application/json" },
+    });
+
+  }
+
+  async function register(){
+    const file = "../../../../public/data/hobbies.csv";
+    // const uid = getCookie("user_id");
+    await fetch(`${process.env.NEXT_PUBLIC_URL}/users/`, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({
+        name: `${fname} ${lname}`,
+        username: username,
+        email: email,
+        password: password,
+        birthdate: new Date(birthday),
+        sex: sex,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setCookie("user_id",data.id);
+    });
 
   }
 
@@ -134,9 +180,19 @@ export default function SignUpPage() {
             <DatePicker
               label="Controlled picker"
               value={dayjs(birthday)}
-              onChange={(e) => birthday.setFullYear(e)}
+              onChange={(e) => {birthday.setFullYear(e);setBirthday(birthday)}}
             />
           </LocalizationProvider>
+          <label style={{ color: "gray", marginTop: 5 }}>Sex</label>
+          <Select
+            value={sex}
+            label="Sex"
+            onChange={(e)=>setSex(e.target.value)}
+          >
+            <MenuItem value={'male'}>Male</MenuItem>
+            <MenuItem value={'female'}>Female</MenuItem>
+            <MenuItem value={'other'}>Other</MenuItem>
+          </Select>
         </>
       ) : null}
       {page == 1 ? (
@@ -210,12 +266,21 @@ export default function SignUpPage() {
         <Button
           variant="contained"
           onClick={() => {
-            page == 0 ? readInterests() : router.push("../../feed/");
-            setPage(page + 1);
+            if(page == 0 && check()){
+              readInterests();
+              register();
+            } else if(check()) {
+              router.push("../../feed/");
+            } 
+            if(page != 0){
+              setHobbies();
+            }
+            // page == 0 ? readInterests() : router.push("../../feed/");
+            check() ? setPage(page + 1) : null;
           }}
           color={page == 0 ? "secondary" : "success"}
         >
-          {page == 0 ? "Next" : "Sign Up"}
+          {page == 0 ? (check() ? "Register Info" : "Missing Info"):("Complete Account")} 
         </Button>
       </div>
     </div>
